ba2ae1de47652ac1844af2e54f53b7e24a09cc4f
Thomas <Thomas@PC-CHAV>
2017-04-29 15:51:55 +0200

    thomas

--- src/metier/Bateau.java
+++ src/metier/Bateau.java
@@ -7,7 +7,7 @@ public class Bateau {
 	private int ptDeVie;
 	private boolean estCoulé;
 	private boolean estVertical;
-	private final int thooomas;
+	private final int thooomasnfjfnbvjfjbvnjbvnfjvbnfjnvkfnvjnjfn;
 
 	
 	public Bateau(String nom, int taille, boolean estVertical) {
